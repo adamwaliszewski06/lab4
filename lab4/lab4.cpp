418b46f8cbc8d5cd817f1d22b37f4943eaf0ad86
06adamw <adam.waliszewski06@gmail.com>
2025-10-28 14:03:50 +0100

    Add project files.

---
+++ lab4/lab4.cpp
@@ -0,0 +1,52 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+/* zad 1 
+int ktora_wieksza(int a, int b);
+void wyswietlanie_wiekszej (int a, int b);
+
+
+int main()
+{
+    int a, b;
+	cout << "Podaj dwie liczby calkowite: ";
+	cin >> a >> b;
+	wyswietlanie_wiekszej(a, b);
+	return 0;
+}
+
+int ktora_wieksza (int a, int b)
+{
+	if (a > b)
+		return a;
+	else
+		return b;
+}
+
+void wyswietlanie_wiekszej(int a, int b) {
+	cout << "Wieksza liczba to: " << ktora_wieksza(a, b) << endl;
+}
+*/
+
+/* zad 2 */
+
+int n; 
+void wypisz_dzielniki(int n);
+
+int main() {
+	wypisz_dzielniki(n);
+}
+
+void wypisz_dzielniki(int n) {
+	cout << "Podaj liczbe dodatnia calkowita: ";
+	cin >> n;
+	cout << "Dzielniki liczby " << n << " to: ";
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) {
+			cout << i << " ";
+		}
+	}
+
+}
